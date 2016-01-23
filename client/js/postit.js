b1426ac59e2b5aa87e13625a2ee9e5b86351dfe7
Mathias Desloges <freakdev@gmail.com>
2016-01-23 18:20:24 +0100

    postit class

--- client/js/postit.js
+++ client/js/postit.js
@@ -4,6 +4,7 @@
 
     var postItClass = {
         instance: null,
+        dom: null,
         init: function (data) {
 
             this.isNew = true;
@@ -11,30 +12,64 @@
                 this.instance = data;
             }
 
+            // if (this.)
+
             return this;
         },
         setInstance: function (data) {
             this.instance = data;
         },
         startEdit: function () {
-
+            this.dom.querySelector('.content div:first-child').style.display = "none";
+            var t = this.dom.querySelector('.content .textarea')
+            t.style.display = "block";
+            t.value = this.instance.content;
         },
         stopEdit: function () {
+            var t = this.dom.querySelector('.content .textarea');
+            t.style.display = 'none';
+            this.dom.querySelector('.content div:first-child').style.display = 'block';
 
-            instance.content = 
+            console.log(t);
 
-            board.newPostIt(instance);
+            this.instance.content = t.querySelector('textarea').value;
+            this.render();
+
+            board.newPostIt(this.instance);
+        },
+        editContent: function (w) {
+            instance.content += " " + w;
+            this.render();
         },
         render: function () {
-            var tpl = document.querySelector('.postit-tpl').innerHTML;
+            var self = this;
 
-            for (var p in this.instance) {
-                tpl = tpl.replace("__" + p + "__", this.instance[p]);
+            if (!this.dom) {
+                var tpl = document.querySelector('.postit-tpl').innerHTML;
+
+                for (var p in this.instance) {
+                    tpl = tpl.replace("__" + p + "__", this.instance[p]);
+                }
+
+                if (this.isNew) {
+                    document.body.innerHTML += tpl;
+                }
+
+                this.dom = document.querySelector('.id-'+this.instance.id);
+
+                this.dom.querySelector('.header').addEventListener('contextmenu', function (e) {
+                    e.preventDefault();
+                    self.startEdit();
+                });
+
+                this.dom.querySelector('button').addEventListener('click', function (e) {
+                    self.stopEdit();
+                });
+
             }
 
-            if (this.isNew) {
-                document.body.innerHTML += tpl;
-            }
+            this.dom.querySelector('.content div').innerHTML = this.instance.content;
+            this.dom.querySelector('.content textarea').value = this.instance.content;
 
             return this;
         }
